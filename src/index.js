9391de98c5e8c38be02b1477c7bad8392ce8ad00
Adam O'Neill <adamjoneill@gmail.com>
2018-01-29 11:20:42 +0000

    remove unused code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@
 (function() {
     // Define our constructor
     this.IdleTimer = function(timeout, callback, options) {
-        // Create global element references
-        this.start = null;
-        this.stop = null;
-
         var timer;
         var resetTimer;
 
@@ -30,17 +26,6 @@
 
         if (options.events === undefined) {
             options.events = defaults.events;
-        }
-
-        // Utility method to extend defaults with user options
-        function extendDefaults(source, properties) {
-            var property;
-            for (property in properties) {
-                if (properties.hasOwnProperty(property)) {
-                    source[property] = properties[property];
-                }
-            }
-            return source;
         }
 
         var start = function() {
@@ -80,41 +65,3 @@
         };
     };
 })();
-
-// var idleTimer = function() {
-//     var start = function(timeout, callback, options) {
-//         var timer;
-//         window.onload = resetTimer;
-//         // DOM Events
-//         if (options === undefined) {
-//             options = {};
-//         }
-//
-//         if (options.events === undefined) {
-//             options.events = [
-//                 "onmousemove",
-//                 "onkeypress",
-//                 "onload",
-//                 "onmousemove",
-//                 "onmousedown",
-//                 "ontouchstart",
-//                 "onclick",
-//                 "onscroll",
-//                 "onkeypress"
-//             ];
-//         }
-//
-//         for (var i = 0; i < options.events.length; i++) {
-//             document[options.events[i]] = resetTimer;
-//         }
-//
-//         function raiseTimeout() {
-//             callback();
-//         }
-//
-//         function resetTimer() {
-//             clearTimeout(timer);
-//             timer = setTimeout(raiseTimeout, timeout);
-//         }
-//     };
-// };
